3524f0fb4b473b10b98e56689d44524997476768
SenX_eb85@itstepru.onmicrosoft.com <SenX_eb85@itstepru.onmicrosoft.com>
2021-09-25 06:21:23 +0500

    25.09.2021

--- Arraya_Task_2/Source.cpp
+++ Arraya_Task_2/Source.cpp
@@ -5,18 +5,31 @@
 using namespace std;
 
 const int n = 11;
-
+const int s = 11;
 void main()
 {
 	setlocale(LC_ALL, "");
-	int v;
-	int arr[n];
+	int v,t;
+	int arr[n],arrS[s];
 	cout << "¬ведите сдвиг число элементов = "; cin >> v;
 	for (int i = 1; i <= 10; i++)
 	{
 		arr[i] = i;
 		cout << arr[i] << " ";
 	}
+	cout << endl;
+	
+	t = 11 - v;
+	
+	for (int i = 1; i <=v; i++)
+	{
+		arrS[i] = t;
+		cout << arrS[i] << " ";
+		t++;
+	}
 
-
+	for (int i = 1; i <=10-v ; i++)
+	{
+		cout << arr[i] << " ";
+	}
 }
